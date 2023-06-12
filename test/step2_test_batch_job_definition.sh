e8ce080d2d0c5780242a045d1a19e73992754c78
dceoy <dnarsil+github@gmail.com>
2023-06-12 22:32:22 +0900

    Add tests for EFS

--- test/step2_test_batch_job_definition.sh
+++ test/step2_test_batch_job_definition.sh
@@ -10,9 +10,12 @@ AWS_REGION="$(aws configure get region)"
 ECR_REGISTRY="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
 BATCH_JOB_ROLE_ARN="${BATCH_JOB_ROLE_ARN:-arn:aws:iam::${AWS_ACCOUNT_ID}:role/${PROJECT_NAME}-BatchJobRole}"
 BATCH_JOB_EXECUTION_ROLE_ARN="${BATCH_JOB_EXECUTION_ROLE_ARN:-arn:aws:iam::${AWS_ACCOUNT_ID}:role/${PROJECT_NAME}-BatchJobExecutionRole}"
-TEST_S3_BUCKET="${INPUT_S3_URI:-${PROJECT_NAME}-output-${AWS_ACCOUNT_ID}}"
-WAIT_SECONDS="${WAIT_SECONDS:-3600}"
-BATCH_SUBMIT_JOB_JSON="${BATCH_SUBMIT_JOB_JSON:-batch/curl.batch.submit-job.j2.json}"
+efs_ap_json="$( \
+  aws efs describe-access-points \
+    | jq ".AccessPoints[] | select(.Name == \"${PROJECT_NAME}-efs-accesspoint\")" \
+)"
+EFS_FS_ID="$(echo "${efs_ap_json}" | jq -r '.FileSystemId')"
+EFS_AP_ID="$(echo "${efs_ap_json}" | jq -r '.AccessPointId')"
 
 echo "PROJECT_NAME:                   ${PROJECT_NAME}"
 echo "IMAGE_NAME:                     ${IMAGE_NAME}"
@@ -22,6 +25,8 @@ echo "AWS_REGION:                     ${AWS_REGION}"
 echo "ECR_REGISTRY:                   ${ECR_REGISTRY}"
 echo "BATCH_JOB_ROLE_ARN:             ${BATCH_JOB_ROLE_ARN}"
 echo "BATCH_JOB_EXECUTION_ROLE_ARN:   ${BATCH_JOB_EXECUTION_ROLE_ARN}"
+echo "EFS_FS_ID:                      ${EFS_FS_ID}"
+echo "EFS_AP_ID:                      ${EFS_AP_ID}"
 
 # oneTimeSetUp() {
 # }
@@ -34,7 +39,7 @@ echo "BATCH_JOB_EXECUTION_ROLE_ARN:   ${BATCH_JOB_EXECUTION_ROLE_ARN}"
 #         --job-definition "${p}-${IMAGE_NAME}:{}"
 #     aws s3 rm --recursive "s3://${TEST_S3_BUCKET}/tmp/${p}-${IMAGE_NAME}/"
 #   done
-#   rm -f *.job-definition.json
+#   rm -f tmp.*.json
 # }
 
 testBatchJobDefinition() {
@@ -44,6 +49,8 @@ testBatchJobDefinition() {
       | jq ".containerProperties.image=\"${ECR_REGISTRY}/${IMAGE_NAME}:${IMAGE_TAG}\"" \
       | jq ".containerProperties.jobRoleArn=\"${BATCH_JOB_ROLE_ARN}\"" \
       | jq ".containerProperties.executionRoleArn=\"${BATCH_JOB_EXECUTION_ROLE_ARN}\"" \
+      | jq ".containerProperties.volumes[0].efsVolumeConfiguration.fileSystemId=\"${EFS_FS_ID}\"" \
+      | jq ".containerProperties.volumes[0].efsVolumeConfiguration.authorizationConfig.accessPointId=\"${EFS_AP_ID}\"" \
       > "tmp.${jdn}.job-definition.json"
     aws batch register-job-definition \
       --cli-input-json "file://tmp.${jdn}.job-definition.json" \
@@ -55,55 +62,6 @@ testBatchJobDefinition() {
           "arn:aws:batch:${AWS_REGION}:${AWS_ACCOUNT_ID}:job-definition/${jdn}:{}" \
         | jq '.jobDefinitions[] | select(.status == "ACTIVE")' \
     )"
-  done
-}
-
-testBatchJobSubmit() {
-  for p in 'fargate' 'ec2'; do
-    if [[ "${p}" == 'ec2' ]]; then
-      job_queue="${PROJECT_NAME}-batch-job-queue-${p}-intel-spot"
-    else
-      job_queue="${PROJECT_NAME}-batch-job-queue-${p}-spot"
-    fi
-    jdn="$(jq -r '.jobDefinitionName' < "tmp.${p}-${IMAGE_NAME}.job-definition.output.json")"
-    jdr="$(jq -r '.revision' < "tmp.${p}-${IMAGE_NAME}.job-definition.output.json")"
-    [[ -n "${jdn}" ]] && [[ -n "${jdr}" ]] || exit 1
-    jq ".jobDefinition=\"${jdn}:${jdr}\"" < "${BATCH_SUBMIT_JOB_JSON}" \
-      | jq ".jobQueue=\"${job_queue}\"" \
-      | jq ".containerOverrides.command[0]=\"--output-s3=s3://${TEST_S3_BUCKET}/tmp/${jdn}/\"" \
-      > "tmp.${jdn}.${BATCH_SUBMIT_JOB_JSON##*/}"
-    aws batch submit-job --cli-input-json "file://tmp.${jdn}.${BATCH_SUBMIT_JOB_JSON##*/}" \
-      | tee "tmp.${jdn}.${BATCH_SUBMIT_JOB_JSON##*/}.output.json"
-    assertEquals 'aws batch submit-job' 0 "${PIPESTATUS[0]}"
-  done
-}
-
-testBatchJobStatus() {
-  end_seconds=$((SECONDS + WAIT_SECONDS))
-  for p in 'fargate' 'ec2'; do
-    ji=$(jq -r '.jobId' < "tmp.${p}-${IMAGE_NAME}.${BATCH_SUBMIT_JOB_JSON##*/}.output.json")
-    [[ -n "${ji}" ]] || exit 1
-    js=''
-    while [[ ${SECONDS} -lt ${end_seconds} ]]; do
-      aws batch describe-jobs --jobs "${ji}" > "tmp.${ji}.batch.describe-jobs.output.json"
-      js=$(jq -r '.jobs[0].status' < "tmp.${ji}.batch.describe-jobs.output.json")
-      [[ -n "${js}" ]] || exit 1
-      if [[ "${js}" == 'SUCCEEDED' ]] || [[ "${js}" == 'FAILED' ]]; then
-        break
-      fi
-      sleep 10
-    done
-    assertEquals 'aws batch describe-jobs' 'SUCCEEDED' "${js}"
-  done
-}
-
-testBatchJobOutput() {
-  for p in 'fargate' 'ec2'; do
-    aws s3 cp \
-      "s3://${TEST_S3_BUCKET}/tmp/${p}-${IMAGE_NAME}/global_ip.txt" \
-      "tmp.${p}-${IMAGE_NAME}.global_ip.txt"
-    assertEquals 'aws s3 cp' 0 "${?}"
-    assertTrue 'aws s3 cp' "[[ -s 'tmp.${p}-${IMAGE_NAME}.global_ip.txt' ]]"
   done
 }
 
---
+++ test/step5_test_batch_efs.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+set -u
+
+PROJECT_NAME="${PROJECT_NAME:-hpc-dev}"
+IMAGE_NAME="${IMAGE_NAME:-test-s3-sync}"
+AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
+TEST_S3_BUCKET="${INPUT_S3_URI:-${PROJECT_NAME}-output-${AWS_ACCOUNT_ID}}"
+TIMEOUT_SECONDS="${TIMEOUT_SECONDS:-3600}"
+BATCH_SUBMIT_JOB_JSON="${BATCH_SUBMIT_JOB_JSON:-batch/df.batch.submit-job.j2.json}"
+
+echo "PROJECT_NAME:                   ${PROJECT_NAME}"
+echo "IMAGE_NAME:                     ${IMAGE_NAME}"
+echo "AWS_ACCOUNT_ID:                 ${AWS_ACCOUNT_ID}"
+echo "TIMEOUT_SECONDS:                ${TIMEOUT_SECONDS}"
+echo "BATCH_SUBMIT_JOB_JSON:          ${BATCH_SUBMIT_JOB_JSON}"
+
+# oneTimeSetUp() {
+# }
+
+# oneTimeTearDown() {
+#   rm -f tmp.*.json
+# }
+
+testBatchJobSubmit() {
+  for p in 'fargate' 'ec2'; do
+    [[ -f "tmp.${p}-${IMAGE_NAME}.job-definition.output.json" ]] || exit 1
+    jdn="$(jq -r '.jobDefinitionName' < "tmp.${p}-${IMAGE_NAME}.job-definition.output.json")"
+    jdr="$(jq -r '.revision' < "tmp.${p}-${IMAGE_NAME}.job-definition.output.json")"
+    if [[ "${p}" == 'ec2' ]]; then
+      job_queue="${PROJECT_NAME}-batch-job-queue-${p}-intel-spot"
+    else
+      job_queue="${PROJECT_NAME}-batch-job-queue-${p}-spot"
+    fi
+    [[ -n "${jdn}" ]] && [[ -n "${jdr}" ]] || exit 1
+    jq ".jobDefinition=\"${jdn}:${jdr}\"" < "${BATCH_SUBMIT_JOB_JSON}" \
+      | jq ".jobQueue=\"${job_queue}\"" \
+      | jq ".containerOverrides.command[0]=\"--output-s3=s3://${TEST_S3_BUCKET}/tmp/${jdn}/\"" \
+      > "tmp.${jdn}.${BATCH_SUBMIT_JOB_JSON##*/}"
+    aws batch submit-job --cli-input-json "file://tmp.${jdn}.${BATCH_SUBMIT_JOB_JSON##*/}" \
+      | tee "tmp.${jdn}.${BATCH_SUBMIT_JOB_JSON##*/}.output.json"
+    assertEquals 'aws batch submit-job' 0 "${PIPESTATUS[0]}"
+  done
+}
+
+testBatchJobStatus() {
+  end_seconds=$((SECONDS + TIMEOUT_SECONDS))
+  for p in 'fargate' 'ec2'; do
+    ji=$(jq -r '.jobId' < "tmp.${p}-${IMAGE_NAME}.${BATCH_SUBMIT_JOB_JSON##*/}.output.json")
+    [[ -n "${ji}" ]] || exit 1
+    js=''
+    while [[ ${SECONDS} -lt ${end_seconds} ]]; do
+      aws batch describe-jobs --jobs "${ji}" > "tmp.${ji}.batch.describe-jobs.output.json"
+      js=$(jq -r '.jobs[0].status' < "tmp.${ji}.batch.describe-jobs.output.json")
+      [[ -n "${js}" ]] || exit 1
+      if [[ "${js}" == 'SUCCEEDED' ]] || [[ "${js}" == 'FAILED' ]]; then
+        break
+      fi
+      sleep 10
+    done
+    assertEquals 'aws batch describe-jobs' 'SUCCEEDED' "${js}"
+  done
+}
+
+testBatchJobOutput() {
+  for p in 'fargate' 'ec2'; do
+    aws s3 cp \
+      "s3://${TEST_S3_BUCKET}/tmp/${p}-${IMAGE_NAME}/df_Th.txt" \
+      "tmp.${p}-${IMAGE_NAME}.df_Th.txt"
+    assertEquals 'aws s3 cp' 0 "${?}"
+    assertNotEquals 'aws s3 cp' 0 \
+      "$(grep -e '/mnt/efs$' "tmp.${p}-${IMAGE_NAME}.df_Th.txt")"
+  done
+}
+
+# shellcheck disable=SC1091
+. shunit2
